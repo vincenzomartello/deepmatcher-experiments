52a7a29888596e75289a26ff449c5b0a528c8e01
Vincenzo <vmartello.95@gmail.com>
2019-12-20 11:52:25 +0100

    added classes for pattern mining and samples building

---
+++ utils/SampleBuilder.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+
+
+
+class SampleBuilder:
+    def __init__(self,comparator):
+        ##string comparator
+        self.comparator == comparator
+
+
+    ##df1 and df2 are two sources of records, attributes are the attributes important for the prediction
+    def createPossibleMatchings(df1,df2,important_attributes,min_similarity,newsample_len):
+        df1_index = list(df1.index)
+        df2_index = list(df2.index)
+        newSamples = []
+        while(len(newSamples)<newsample_len):
+            current_lrow = df1.iloc[rd.choice(df1.index)]
+            current_df = df2
+            for att in important_attributes:
+                l_att = str(current_lrow[att])
+                mask = current_df.apply(lambda row: self.comparator.similarity(str(row[att]),l_att)
+                                        >=min_similarity,axis=1)
+                current_df = current_df[mask]
+            for idx in current_df.index:
+                newSamples.append((current_lrow['id'],current_df.at[idx,'id']))
+        newSamples_ids = pd.DataFrame(data=newSamples,columns=['ltable_id','rtable_id'])
+        left_columns = list(map(lambda s:left_prefix+s,list(df1)))
+        right_columns = list(map(lambda s:right_prefix+s,list(df2)))
+        df1.columns = left_columns
+        df2.columns = right_columns
+
+        #P sta per parziale
+        punlabeled = pd.merge(newSamples_ids,df1, how='inner')
+        unlabeled_df = pd.merge(punlabeled,df2,how='inner')
+
+        unlabeled_df = unlabeled_df.drop(['ltable_id','rtable_id'],axis=1)
+        unlabeled_df['id'] = np.arange(unlabeled_df.shape[0])
+        return unlabeled_df
+
+
+    ##new attribute value is a couple of attributes
+    def buildNewSamples(dataset,selectedAttr,newAttributeVal,newSamples_len,label,left_prefix='ltable_',
+                       right_prefix='rtable_'):
+
+        new_samples = pd.DataFrame(data = [], columns =list(dataset))
+        for i in range(newSamples_len):
+            selected_row = dataset.sample()
+            if label==0:
+                selected_row[left_prefix+selectedAttr] = newAttributeVal[0]
+                selected_row[right_prefix+selectedAttr] = newAttributeVal[1]
+            else:
+                selected_row[left_prefix+selectedAttr] = selected_row[left_prefix+selectedAttr]+" "+newAttributeVal[0]
+                selected_row[right_prefix+selectedAttr] = selected_row[right_prefix+selectedAttr]+" "+newAttributeVal[1]
+            new_samples = new_samples.append(selected_row,ignore_index=True)
+        return new_samples
+
+
+
+    def buildNewSamplesForAttribute(critical_forPos,critical_forNeg,attribute,lenNewPositives,lenNewNegatives,
+                                   start_idx):
+        newSamples = []
+        for df,_,_ in critical_forPos[attribute]:
+            if df.shape[0] < lenNewPositives:
+                newSamples.append(df)
+            else:
+                newSamples.append(df.sample(n= lenNewPositives))
+        for df,_,_ in critical_forNeg[attribute]:
+            if df.shape[0] < lenNewNegatives:
+                newSamples.append(df)
+            else:
+                newSamples.append(df.sample(n= lenNewNegatives))
+        newSamples = pd.concat(newSamples)
+        newSamples = newSamples.drop(columns=['match_score'])
+        newSamples['id'] = np.arange(start_idx,start_idx+newSamples.shape[0])
+        return newSamples
--- utils/explainer.py
+++ utils/explainer.py
@@ -4,18 +4,18 @@ import os
 
 
 class Explainer:
-    def __init__(self,k,model,testset_path,temp_path='temp',left_prefix='ltable_',right_prefix='rtable_'):
+    def __init__(self,model,k,temp_path='temp',left_prefix='ltable_',right_prefix='rtable_'):
+        ##k is the number of explanations to provide
         self.k = k
         self.model = model
-        self.testset_path = testset_path
         self.temp_path = temp_path
-        self.left_prefix = left_prefix
-        self.right_prefix = right_prefix
+        self.leftPrefix = left_prefix
+        self.rightPrefix = right_prefix
         
             
     #explanation nr is the number of neighbors to consider
-    def generateExplanations(nearest_neighbors,opposite_label_df,
-                             attribute,threshold,true_label):
+    def generateExplanations(self,nearest_neighbors,opposite_label_df,
+                             attribute,threshold,testsetPath,true_label):
 
         nn_frequencies = nearest_neighbors[attribute].value_counts().rename_axis('unique_values').reset_index(name='counts')
         nearest_neighbors_values = pd.merge(nn_frequencies,opposite_label_df,left_on='unique_values',right_on='id')
@@ -24,7 +24,7 @@ class Explainer:
             explanation_nr = selected_cols.shape[0]
         else:
             explanation_nr = self.k
-        testset = dm.data.process_unlabeled(self.testset_path,self.model,ignore_columns=['id','label'])
+        testset = dm.data.process_unlabeled(testsetPath,self.model,ignore_columns=['id','label'])
         standard_preds = self.model.run_prediction(testset)
         if (true_label == 1):
             original_false_negatives = standard_preds[standard_preds['match_score'] <= 0.5].shape[0]
@@ -33,7 +33,7 @@ class Explainer:
         most_frequent_values = selected_cols.head(explanation_nr)
         falseNegatives = []
         for i in range(explanation_nr):
-            testset = pd.read_csv(self.testset_path)
+            testset = pd.read_csv(testsetPath)
             lval = str(most_frequent_values.loc[i][self.leftPrefix+attribute])
             rval = str(most_frequent_values.loc[i][self.rightPrefix+attribute])
             testset[self.leftPrefix+attribute] = lval
@@ -54,7 +54,7 @@ class Explainer:
 
 
     ## this function return occurrences in negatives and positives samples
-    def analyze_valueDistribution(dataset,value,attribute):
+    def analyze_valueDistribution(self,dataset,value,attribute):
         dataset_pos = dataset[dataset['label']==1]
         dataset_neg = dataset[dataset['label']==0]
 
@@ -75,22 +75,25 @@ class Explainer:
     
     ##Given an attribute pair return the true positives calculated from the model for the attribute pair
     ## if the attribute pair is inserted in place of original values
-    def testRobustness(testset_path,attribute,true_label,substitute_values):
+    def testRobustness(self,testset_path,true_label,attribute,substitute_values,strategy):
         standard_test = dm.data.process_unlabeled(testset_path,self.model,ignore_columns=['id','label'])
-        standard_pred = model.run_prediction(standard_test)
+        standard_pred = self.model.run_prediction(standard_test)
         test_df = pd.read_csv(testset_path)
         if true_label == 1:
             original_true_pos = standard_pred[standard_pred.match_score>0.5].shape[0]
         else:
             original_true_pos = standard_pred[standard_pred.match_score<=0.5].shape[0]
-        lval = substitute_values[0]
-        rval = substitute_values[1]
-        test_df['ltable_'+attribute] = lval
-        test_df['rtable_'+attribute] = rval
+            
+        if strategy=='replace':
+            test_df[self.leftPrefix+attribute] = substitute_values[0]
+            test_df[self.rightPrefix+attribute] = substitute_values[1]
+        elif strategy=='concat':
+            test_df[self.leftPrefix+attribute] = test_df[self.leftPrefix+attribute].astype(str)+" "+substitute_values[0]
+            test_df[self.rightPrefix+attribute] = test_df[self.rightPrefix+attribute].astype(str)+" "+substitute_values[1]
         test_df.to_csv(os.path.join(self.temp_path,'new_test.csv'),index=False)
         new_test = dm.data.process_unlabeled(os.path.join(self.temp_path,'new_test.csv'),self.model
                                              ,ignore_columns=['id','label'])
-        new_pred = model.run_prediction(new_test,output_attributes=True)
+        new_pred = self.model.run_prediction(new_test,output_attributes=True)
         if true_label ==1:
             new_true_pos = new_pred[new_pred.match_score >0.5].shape[0]
         else:
