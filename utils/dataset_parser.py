621c762b2ee4a85533f26164732ea6997d90a1fa
vincenzomartello <vmartello.95@gmail.com>
2020-02-24 19:12:17 +0100

    fixed typo

--- utils/dataset_parser.py
+++ utils/dataset_parser.py
@@ -21,7 +21,7 @@ def generate_train_valid_test(dataset_dir,splitfiles,left_prefix,right_prefix,dr
     #P sta per parziale
     for dataset_id in datasets_ids:
         pdata = pd.merge(dataset_id,df_tableA, how='inner',left_on='ltable_id',right_on=left_prefix+'id')
-        dataset = pd.merge(pdata,df_tableB,how='inner',left_on='rtable_id','right_on=right_prefix+'id')
+        dataset = pd.merge(pdata,df_tableB,how='inner',left_on='rtable_id',right_on=right_prefix+'id')
         datasets.append(dataset)
 
     train_lenght = datasets[0].shape[0]
@@ -45,25 +45,10 @@ def generate_train_valid_test(dataset_dir,splitfiles,left_prefix,right_prefix,dr
     return train,valid,test
 
 
-def generate_pos_neg_datasets(dataset_dir,train_name,valid_name,test_name):
-    train_df = pd.read_csv(os.path.join(dataset_dir,train_name))
-    validation_df = pd.read_csv(os.path.join(dataset_dir,valid_name))
-    test_df = pd.read_csv(os.path.join(dataset_dir,test_name))
-
-    train_negatives = train_df[train_df['label']==0]
-    validation_negatives = validation_df[validation_df['label']==0]
-    test_negatives = test_df[test_df['label']==0]
-
-    train_positives = train_df[train_df['label']==1]
-    validation_positives = validation_df[validation_df['label']==1]
-    test_positives = test_df[test_df['label']==1]
-
-    negatives_df = train_negatives.append(validation_negatives,ignore_index=True)
-    negatives_df = negatives_df.append(test_negatives,ignore_index=True)
-
-    positives_df = train_positives.append(validation_positives,ignore_index=True)
-    positives_df = positives_df.append(test_positives,ignore_index=True)
-
+def generate_pos_neg_datasets(splits):
+    allSamples = pd.concat(splits,ignore_index=True)
+    positives_df = allSamples[allSamples.label==1]
+    negatives_df = allSamples[allSamples.label==0]
     return (positives_df,negatives_df)
 
 
@@ -85,22 +70,5 @@ def generate_unlabeled(dataset_dir,unlabeled_filename,left_prefix='ltable_',righ
     return unlabeled_df
 
 
-def getFullDataset(dataset_dir,splitfiles,left_prefix,right_prefix):
-    df_tableA = pd.read_csv(os.path.join(dataset_dir,'tableA.csv'))
-    df_tableB = pd.read_csv(os.path.join(dataset_dir,'tableB.csv'))
-    dataset_splits = []
-    for splitname in splitfiles:
-        dataset_splits.append(pd.read_csv(os.path.join(dataset_dir,splitname)))
-    left_columns = []
-    right_columns = []
-    for lcol,rcol in zip(list(df_tableA),list(df_tableB)):
-        left_columns.append(left_prefix+lcol)
-        right_columns.append(right_prefix+rcol)
-    df_tableA.columns = left_columns
-    df_tableB.columns = right_columns
-    allData = pd.concat(dataset_splits)
-    #P sta per parziale
-    pdata =pd.merge(allData,df_tableA, how='inner')
-    dataset_df =pd.merge(pdata,df_tableB,how='inner')
-    dataset_df['id'] = np.arange(len(dataset_df))
-    return dataset_df
+def getFullDataset(splits):
+    return pd.concat(splits,ignore_index=True)
